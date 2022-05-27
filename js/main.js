e9d5a047feb91860f72afd1003223454e0601395
psorrells <pamelan.sorrells@gmail.com>
2022-05-26 19:40:03 -0500

    coin flip functioning on server side

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 document.querySelector('button').addEventListener('click', flipCoin)
 
 async function flipCoin() {
-    let flipping = true
+    const head = document.querySelector('.head')
+    const tail = document.querySelector('.tail')
 
+    head.style.transform = 'rotateY(0deg)'
+    tail.style.transform = 'rotateY(180deg)'
+
+    await new Promise(r => setTimeout(r, 1000))
+    
     let result,data;
-
     try {
     data = await fetch('http://localhost:5000/coinflip', {
         "method": "GET"
@@ -16,11 +21,13 @@ async function flipCoin() {
 
     if (result.message === "heads") {
         //flip coin to heads up
-        flipping = false
+        head.style.transform = 'rotateY(720deg)'
+        tail.style.transform = 'rotateY(900deg)'
         console.log('heads!')
     } else {
         //flip coin to tails up
-        flipping = false
+        tail.style.transform = 'rotateY(1440deg)'
+        head.style.transform = 'rotateY(1260deg)'
         console.log('tails!')
     }
     return null
